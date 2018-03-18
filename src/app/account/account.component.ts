6d6e08fc296fd3505e58e12c772fae1663f097d2
sreekanth.pullat <sreekanth.pullat@visualiq.com>
2018-03-18 22:10:16 +0530

    Injectable services

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -5,19 +5,16 @@ import { AccountService } from '../account.service';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
 
-  constructor(private loggingService: LoggingService, 
-  private accountService: AccountService){}
+  constructor(private accountService: AccountService){}
 
 
   onSetTo(status: string) {
     this.accountService.uodateStatus(this.id, status);
-    this.loggingService.logStatus(status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -5,17 +5,13 @@ import { AccountService } from '../account.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {  
 
-  constructor(
-    private loggingService: LoggingService, 
-    private accountService: AccountService){}
+  constructor(private accountService: AccountService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, status);
-    this.loggingService.logStatus(accountStatus);
   }
 }
